1fc0d144962164a4bee5c277efd0a53918d88d96
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-11-29 10:58:51 +1300

    add the rest of soil variables

--- output_challenge/visualise.R
+++ output_challenge/visualise.R
@@ -34,3 +34,105 @@ ggplot(soil_c_pool_median) +
        y = expression(paste("Soil C [kg ", m^{-3}, "]"))) +
   theme_bw() +
   theme(legend.key.width = unit(1, "cm"))
+
+# soil nitrogen
+var_names_soil_n_pool <- var_names[str_detect(var_names, "soil_n_pool_")]
+soil_n_pool <- lapply(var_names_soil_n_pool, \(x) get_var(out, x))
+soil_n_pool_total <- apply(simplify2array(soil_n_pool), 1:2, sum)
+soil_n_pool <- c(soil_n_pool, list(soil_n_pool_total))
+soil_n_pool_median <- lapply(soil_n_pool, \(x) apply(x, 2, median))
+names(soil_n_pool_median) <- c(var_names_soil_n_pool, "soil_n_pool_total")
+soil_n_pool_median <-
+  reshape2::melt(soil_n_pool_median) %>%
+  rename(Pool = L1) %>%
+  mutate(Pool = str_remove(Pool, "soil_n_pool_")) %>%
+  group_by(Pool) %>%
+  mutate(time = row_number())
+
+ggplot(soil_n_pool_median) +
+  geom_line(aes(time, value,
+                colour = Pool,
+                linetype = Pool),
+            size = 1) +
+  scale_y_sqrt() +
+  scale_colour_viridis_d(option = "cividis") +
+  labs(x = "Time step [month]",
+       y = expression(paste("Soil N [kg ", m^{-3}, "]"))) +
+  theme_bw() +
+  theme(legend.key.width = unit(1, "cm"))
+
+# soil phosphorous
+var_names_soil_p_pool <- var_names[str_detect(var_names, "soil_p_pool_")]
+soil_p_pool <- lapply(var_names_soil_p_pool, \(x) get_var(out, x))
+soil_p_pool_total <- apply(simplify2array(soil_p_pool), 1:2, sum)
+soil_p_pool <- c(soil_p_pool, list(soil_p_pool_total))
+soil_p_pool_median <- lapply(soil_p_pool, \(x) apply(x, 2, median))
+names(soil_p_pool_median) <- c(var_names_soil_p_pool, "soil_p_pool_total")
+soil_p_pool_median <-
+  reshape2::melt(soil_p_pool_median) %>%
+  rename(Pool = L1) %>%
+  mutate(Pool = str_remove(Pool, "soil_p_pool_")) %>%
+  group_by(Pool) %>%
+  mutate(time = row_number())
+
+ggplot(soil_p_pool_median) +
+  geom_line(aes(time, value,
+                colour = Pool,
+                linetype = Pool),
+            size = 1) +
+  scale_y_sqrt() +
+  scale_colour_viridis_d(option = "cividis") +
+  labs(x = "Time step [month]",
+       y = expression(paste("Soil P [kg ", m^{-3}, "]"))) +
+  theme_bw() +
+  theme(legend.key.width = unit(1, "cm"))
+
+# soil enzyme
+var_names_soil_enzyme <- var_names[str_detect(var_names, "soil_enzyme_")]
+soil_enzyme <- lapply(var_names_soil_enzyme, \(x) get_var(out, x))
+soil_enzyme_total <- apply(simplify2array(soil_enzyme), 1:2, sum)
+soil_enzyme <- c(soil_enzyme, list(soil_enzyme_total))
+soil_enzyme_median <- lapply(soil_enzyme, \(x) apply(x, 2, median))
+names(soil_enzyme_median) <- c(var_names_soil_enzyme, "soil_enzyme_total")
+soil_enzyme_median <-
+  reshape2::melt(soil_enzyme_median) %>%
+  rename(Pool = L1) %>%
+  mutate(Pool = str_remove(Pool, "soil_enzyme_")) %>%
+  group_by(Pool) %>%
+  mutate(time = row_number())
+
+ggplot(soil_enzyme_median) +
+  geom_line(aes(time, value,
+                colour = Pool,
+                linetype = Pool),
+            size = 1) +
+  scale_y_sqrt() +
+  scale_colour_viridis_d(option = "cividis") +
+  labs(x = "Time step [month]",
+       y = expression(paste("Soil enzyme [kg C ", m^{-3}, "]"))) +
+  theme_bw() +
+  theme(legend.key.width = unit(1, "cm"))
+
+# soil abiotic
+var_names_soil_abiotic <-
+  c("soil_temperature", "soil_moisture", "soil_evaporation")
+soil_abiotic <- lapply(var_names_soil_abiotic, \(x) get_var(out, x))
+# get soil layer 13
+soil_abiotic[[1]] <- soil_abiotic[[1]][, 13, ]
+soil_abiotic[[2]] <- soil_abiotic[[2]][, 13, ]
+soil_abiotic_median <- lapply(soil_abiotic, \(x) apply(x, 2, median))
+names(soil_abiotic_median) <- var_names_soil_abiotic
+soil_abiotic_median <-
+  reshape2::melt(soil_abiotic_median) %>%
+  rename(Variable = L1) %>%
+  mutate(Variable = str_remove(Variable, "soil_")) %>%
+  group_by(Variable) %>%
+  mutate(time = row_number())
+
+ggplot(soil_abiotic_median) +
+  facet_wrap(~ Variable,
+             scales = "free_y") +
+  geom_line(aes(time, value),
+            size = 1) +
+  labs(x = "Time step [month]") +
+  theme_bw()
