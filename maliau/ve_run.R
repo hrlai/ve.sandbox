22a164f75f42852265b53961bd3ab0f2537837ac
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-16 14:38:14 +0800

    incremental update

--- maliau/ve_run.R
+++ maliau/ve_run.R
@@ -1,7 +1,5 @@
 library(ve.utils)
 
 # run VE
-# ve_run maliau/config --out maliau/out_dev --logfile maliau/out_dev/logfile.log --config core.debug.truncate_run_at_update=60
-ve_run("maliau/config",
-       "maliau/out",
-       "maliau/out/logfile.log")
+# ve_run maliau/config_dev --out maliau/out_dev --logfile maliau/out_dev/logfile.log --config core.debug.truncate_run_at_update=2
+ve_run("maliau/config", "maliau/out", "maliau/out/logfile.log")
