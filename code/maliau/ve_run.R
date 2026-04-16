64c82346f109ff22c4a55fd76116acb3cb2b27b4
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-16 21:31:17 +0800

    update command line

--- code/maliau/ve_run.R
+++ code/maliau/ve_run.R
@@ -1,5 +1,6 @@
 library(ve.utils)
 
 # run VE
-# ve_run maliau/config_dev --out maliau/out_dev --logfile maliau/out_dev/logfile.log --config core.debug.truncate_run_at_update=2
+# ve_run config/maliau_2 --out out/maliau_2 --logfile out/maliau_2/logfile.log
+
 ve_run("maliau/config", "maliau/out", "maliau/out/logfile.log")
